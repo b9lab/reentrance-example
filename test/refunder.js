4ad4dd4426948b24ed1873aebcfeae6ac0508e5c
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-06-22 17:33:54 +0200

    Stricter testing and syntax.

--- test/refunder.js
+++ test/refunder.js
@@ -1,24 +1,24 @@
-var RefunderGood = artifacts.require("./RefunderGood.sol");
-var RefunderBad = artifacts.require("./RefunderBad.sol");
-var Attacker = artifacts.require("./Attacker.sol");
+const RefunderGood = artifacts.require("./RefunderGood.sol");
+const RefunderBad = artifacts.require("./RefunderBad.sol");
+const Attacker = artifacts.require("./Attacker.sol");
 
 Extensions = require("../utils/extensions.js");
 Extensions.init(web3, assert);
 
 contract('Refunders', function(accounts) {
 
-    var refunderTypes = [
+    const refunderTypes = [
         "RefunderGood",
         "RefunderBad"
     ];
-    var owner, victim;
+    let owner, victim;
 
     before("should have proper accounts", function() {
-        assert.isAtLeast(accounts.length, 1, "should have at least 1 account");
-        owner = accounts[0];
-        victim = accounts[1];
+        assert.isAtLeast(accounts.length, 2, "should have at least 2 accounts");
+        owner = accounts[ 0 ];
+        victim = accounts[ 1 ];
         return Extensions.makeSureAreUnlocked(
-                [ owner, victim ])
+            [ owner, victim ])
             .then(() => Extensions.makeSureHasAtLeast(
                 owner,
                 [ victim ],
@@ -28,25 +28,25 @@ contract('Refunders', function(accounts) {
     refunderTypes.forEach(refunderType => {
 
         describe(refunderType, function() {
-        
-            var refunderTypeCopy = refunderType;
-            var refunderName = refunderTypeCopy;
-            var RefunderType;
+
+            const refunderTypeCopy = refunderType;
+            const refunderName = refunderTypeCopy;
+            let RefunderType;
 
             before("should prepare contract types", function() {
                 // We have to do it here otherwise contracts are not defined
-                switch(refunderTypeCopy) {
+                switch (refunderTypeCopy) {
                     case "RefunderGood":
                         RefunderType = RefunderGood;
                         break;
                     case "RefunderBad":
                         RefunderType = RefunderBad;
-                            break;
+                        break;
                 }
             });
 
             describe("should work as expected when simple", function() {
-                var refunder;
+                let refunder;
 
                 beforeEach("should deploy a " + refunderName, function() {
                     return RefunderType.new({ from: owner })
@@ -55,9 +55,9 @@ contract('Refunders', function(accounts) {
 
                 beforeEach("should give to owner and victim", function() {
                     return Promise.all([
-                            refunder.refundIt(owner, { from: owner, value: web3.toWei(1, "finney") }),
-                            refunder.refundIt(victim, { from: owner, value: web3.toWei(2, "finney") })
-                        ])
+                        refunder.refundIt(owner, { from: owner, value: web3.toWei(1, "finney") }),
+                        refunder.refundIt(victim, { from: owner, value: web3.toWei(2, "finney") })
+                    ])
                         .then(txObjects => Promise.all([
                             web3.eth.getBalancePromise(refunder.address),
                             refunder.getPaymentOf(owner),
@@ -65,15 +65,15 @@ contract('Refunders', function(accounts) {
                         ]))
                         .then(payments => {
                             assert.strictEqual(
-                                payments[0].toString(10),
+                                payments[ 0 ].toString(10),
                                 web3.toWei(3, "finney").toString(10),
                                 "should have received correct balance");
                             assert.strictEqual(
-                                payments[1].toString(10),
+                                payments[ 1 ].toString(10),
                                 web3.toWei(1, "finney").toString(10),
                                 "should have marked as received");
                             assert.strictEqual(
-                                payments[2].toString(10),
+                                payments[ 2 ].toString(10),
                                 web3.toWei(2, "finney").toString(10),
                                 "should have marked as received");
                         });
@@ -119,7 +119,7 @@ contract('Refunders', function(accounts) {
             });
 
             describe("should work as expected when attacked but attacker is last", function() {
-                var refunder, attacker;
+                let refunder, attacker;
 
                 beforeEach("should deploy a " + refunderName, function() {
                     return RefunderType.new({ from: owner })
@@ -137,9 +137,9 @@ contract('Refunders', function(accounts) {
 
                 beforeEach("should give to attacker and victim", function() {
                     return Promise.all([
-                            refunder.refundIt(attacker.address, { from: owner, value: web3.toWei(1, "finney") }),
-                            refunder.refundIt(victim, { from: owner, value: web3.toWei(2, "finney") })
-                        ])
+                        refunder.refundIt(attacker.address, { from: owner, value: web3.toWei(1, "finney") }),
+                        refunder.refundIt(victim, { from: owner, value: web3.toWei(2, "finney") })
+                    ])
                         .then(txObjects => Promise.all([
                             web3.eth.getBalancePromise(refunder.address),
                             refunder.getPaymentOf(attacker.address),
@@ -147,15 +147,15 @@ contract('Refunders', function(accounts) {
                         ]))
                         .then(payments => {
                             assert.strictEqual(
-                                payments[0].toString(10),
+                                payments[ 0 ].toString(10),
                                 web3.toWei(3, "finney").toString(10),
                                 "should have received correct balance");
                             assert.strictEqual(
-                                payments[1].toString(10),
+                                payments[ 1 ].toString(10),
                                 web3.toWei(1, "finney").toString(10),
                                 "should have marked as received");
                             assert.strictEqual(
-                                payments[2].toString(10),
+                                payments[ 2 ].toString(10),
                                 web3.toWei(2, "finney").toString(10),
                                 "should have marked as received");
                         });
@@ -182,8 +182,8 @@ contract('Refunders', function(accounts) {
             });
 
             describe("should behave differently when attacker is first", function() {
-                var refunder, attacker;
-                var victimPayment = 205;
+                let refunder, attacker;
+                const victimPayment = 200;
 
                 before("should deploy a " + refunderName, function() {
                     return RefunderType.new({ from: owner })
@@ -192,7 +192,7 @@ contract('Refunders', function(accounts) {
 
                 before("should deploy an attacker", function() {
                     return Attacker.new(refunder.address, { from: owner })
-                        .then(function (created) {
+                        .then(created => {
                             attacker = created;
                             return attacker.attacked();
                         })
@@ -201,9 +201,9 @@ contract('Refunders', function(accounts) {
 
                 before("should give to attacker and victim", function() {
                     return Promise.all([
-                            refunder.refundIt(attacker.address, { from: owner, value: web3.toWei(1, "finney") }),
-                            refunder.refundIt(victim, { from: owner, value: web3.toWei(victimPayment, "finney") })
-                        ])
+                        refunder.refundIt(attacker.address, { from: owner, value: web3.toWei(1, "finney") }),
+                        refunder.refundIt(victim, { from: owner, value: web3.toWei(victimPayment, "finney") })
+                    ])
                         .then(txObjects => Promise.all([
                             web3.eth.getBalancePromise(refunder.address),
                             refunder.getPaymentOf(attacker.address),
@@ -211,20 +211,20 @@ contract('Refunders', function(accounts) {
                         ]))
                         .then(payments => {
                             assert.strictEqual(
-                                payments[0].toString(10),
+                                payments[ 0 ].toString(10),
                                 web3.toWei(victimPayment + 1, "finney").toString(10),
                                 "should have received correct balance");
                             assert.strictEqual(
-                                payments[1].toString(10),
+                                payments[ 1 ].toString(10),
                                 web3.toWei(1, "finney").toString(10),
                                 "should have marked as received");
                             assert.strictEqual(
-                                payments[2].toString(10),
+                                payments[ 2 ].toString(10),
                                 web3.toWei(victimPayment, "finney").toString(10),
                                 "should have marked as received");
                         });
                 });
-                
+
                 if (refunderTypeCopy === "RefunderGood") {
 
                     it("should resist attack", function() {
@@ -251,6 +251,7 @@ contract('Refunders', function(accounts) {
                 if (refunderTypeCopy === "RefunderBad") {
 
                     it("should fail under attack", function() {
+                        this.slow(5000);
                         return attacker.attack({ from: owner, gas: 3000000 })
                             .then(txObject => {
                                 // Not making too many tests...
@@ -258,9 +259,9 @@ contract('Refunders', function(accounts) {
                                 return web3.eth.getBalancePromise(refunder.address);
                             })
                             .then(balance => assert.strictEqual(
-                                balance.toString(10),
-                                web3.toWei(1, "finney").toString(10),
-                                "should have drained more than supposed"));
+                                balance.toNumber(),
+                                0,
+                                "should have drained the whole contract"));
                     });
 
                 }
